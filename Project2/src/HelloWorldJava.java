7ee0d2e826b99a3522ae67ca197be3d2c05c65f1
SaiPriyanka <priyanka@192.168.2.143>
2016-02-22 23:05:58 -0800

    Adding project

---
+++ Project2/src/HelloWorldJava.java
@@ -0,0 +1,24 @@
+
+
+/***
+ * 
+ * Helloworld
+ * @author priyanka
+ *
+ */
+public class HelloWorldJava {
+	
+	public void main(String[] args){
+		print("Hello World");
+		//Adding Comment to test diff
+		print("Hello!!!!!This is Project3");
+		//Added comment
+		
+		
+	}
+	
+	public void print(String text){
+		System.out.println(text+"Testing");
+	}
+
+}
