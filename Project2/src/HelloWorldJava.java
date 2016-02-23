60da8f44d7b823eacb67eca28d705f1468494d22
SaiPriyanka <priyanka@192.168.2.143>
2016-02-23 09:54:16 -0800

    added new method

--- Project2/src/HelloWorldJava.java
+++ Project2/src/HelloWorldJava.java
@@ -9,16 +9,22 @@
 public class HelloWorldJava {
 	
 	public void main(String[] args){
-		print("MAster branch  #######");
-		//Adding Comment to test diff
+		print("Hello Git");
 		print("Hello!!!!!This is Project3");
-		//Added comment
+		
 		
 		
 	}
-	
+	/**
+	 * This prints the information for better logging
+	 * @param text
+	 */
 	public void print(String text){
 		System.out.println(text+"Testing");
+	}
+	
+	public void print(String text,String text1){
+		System.out.println(text+"Testing"+text1);
 	}
 
 }
