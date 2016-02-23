84aaa34cb3bfc50fb780051c256c7b21169fe33f
SaiPriyanka <priyanka@192.168.2.143>
2016-02-23 00:02:46 -0800

    added print stement

--- Project2/src/HelloWorldJava.java
+++ Project2/src/HelloWorldJava.java
@@ -10,6 +10,8 @@ public class HelloWorldJava {
 	
 	public void main(String[] args){
 		print("Working on Future BRanch");
+		print("Working on Future BRanch");
+		
 		//Adding Comment to test diff
 		print("Hello!!!!!This is Project3");
 		//Added comment
