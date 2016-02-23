226d83d3b45e69898b28f97d9a2684dcbcda223d
SaiPriyanka <priyanka@192.168.2.143>
2016-02-23 00:04:21 -0800

    updated prnt

--- Project2/src/HelloWorldJava.java
+++ Project2/src/HelloWorldJava.java
@@ -9,7 +9,7 @@
 public class HelloWorldJava {
 	
 	public void main(String[] args){
-		print("MAster branch");
+		print("MAster branch  #######");
 		//Adding Comment to test diff
 		print("Hello!!!!!This is Project3");
 		//Added comment
